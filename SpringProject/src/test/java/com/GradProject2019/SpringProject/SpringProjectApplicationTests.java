5a251f437d28828c5205ed44af65762d8eb189a1
Grace <g.burleyjones@elsevier.com>
2019-10-07 11:58:33 +0100

    GBJ: GP19-38

---
+++ SpringProject/src/test/java/com/GradProject2019/SpringProject/SpringProjectApplicationTests.java
@@ -0,0 +1,16 @@
+package com.GradProject2019.SpringProject;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringProjectApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
