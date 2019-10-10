a7acac1685bad20c8bc30903772fd7b70ad1ff06
Karen <k.zamani-pickford@elsevier.com>
2019-10-10 11:00:47 +0100

    fixed indentation

--- SpringProject/src/main/resources/db/migration/V1__create_schema.sql
+++ SpringProject/src/main/resources/db/migration/V1__create_schema.sql
@@ -1,10 +1,10 @@
 CREATE TABLE conference (
-id BIGINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-conference_name VARCHAR(255),
-conference_date_time DATETIME,
-city VARCHAR(255),
-conference_description TEXT,
-conference_topic VARCHAR(255)
+    id BIGINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    conference_name VARCHAR(255),
+    conference_date_time DATETIME,
+    city VARCHAR(255),
+    conference_description TEXT,
+    conference_topic VARCHAR(255)
 );
 
 
