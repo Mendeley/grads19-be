f9d9155a65ea530ab7424ca8386ba2efe205abdf
Karen <k.zamani-pickford@elsevier.com>
2019-10-11 14:02:03 +0100

    created controller layer

--- SpringProject/src/main/java/com/gradproject2019/Controller.java
+++ SpringProject/src/main/java/com/gradproject2019/Controller.java
@@ -1,13 +1,20 @@
 package com.gradproject2019;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
 
 @org.springframework.stereotype.Controller
-@RequestMapping(path="/all")
+//@RequestMapping(path="/all")
 public class Controller {
 
     @Autowired
     private ConferenceRepository conferenceRepository;
+
+    @GetMapping(path="/ListOfConferences")
+    public @ResponseBody Iterable<Conference> getAllUsers(){
+        return conferenceRepository.findAll();
+    }
 
 }
