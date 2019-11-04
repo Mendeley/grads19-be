44d4508fb758828dfedbbdbf12bde8455da13c34
MerrettM <m.merrett@elsevier.com>
2019-11-04 17:19:43 +0000

    GP19-67/68/69: create user request and response dtos

---
+++ SpringProject/src/main/java/com/gradproject2019/users/data/UserRequestDto.java
@@ -0,0 +1,124 @@
+package com.gradproject2019.users.data;
+
+import com.gradproject2019.users.persistance.User;
+import org.springframework.validation.annotation.Validated;
+
+import javax.validation.constraints.NotNull;
+
+import static com.gradproject2019.users.persistance.User.UserBuilder.anUser;
+
+@Validated
+public class UserRequestDto {
+    @NotNull(message = "Invalid entry in user field.")
+    private String username;
+
+    @NotNull(message = "Invalid entry in user field.")
+    private String firstName;
+
+    @NotNull(message = "Invalid entry in user field.")
+    private String lastName;
+
+    @NotNull(message = "Invalid entry in user field.")
+    private String email;
+
+    @NotNull(message = "Invalid entry in user field.")
+    private String password;
+
+    @NotNull(message = "Invalid entry in user field.")
+    private String occupation;
+
+    public UserRequestDto() {
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getOccupation() {
+        return occupation;
+    }
+
+    public User from(UserRequestDto userRequestDto) {
+        return anUser()
+                .withUsername(userRequestDto.getUsername())
+                .withFirstName(userRequestDto.getFirstName())
+                .withLastName(userRequestDto.getLastName())
+                .withEmail(userRequestDto.getEmail())
+                .withPassword(userRequestDto.getPassword())
+                .withOccupation(userRequestDto.getOccupation())
+                .build();
+    }
+
+    public static final class UserRequestDtoBuilder {
+        private String username;
+        private String firstName;
+        private String lastName;
+        private String email;
+        private String password;
+        private String occupation;
+
+        private UserRequestDtoBuilder() {
+        }
+
+        public static UserRequestDtoBuilder anUserRequestDto() {
+            return new UserRequestDtoBuilder();
+        }
+
+        public UserRequestDtoBuilder withUsername(String username) {
+            this.username = username;
+            return this;
+        }
+
+        public UserRequestDtoBuilder withFirstName(String firstName) {
+            this.firstName = firstName;
+            return this;
+        }
+
+        public UserRequestDtoBuilder withLastName(String lastName) {
+            this.lastName = lastName;
+            return this;
+        }
+
+        public UserRequestDtoBuilder withEmail(String email) {
+            this.email = email;
+            return this;
+        }
+
+        public UserRequestDtoBuilder withPassword(String password) {
+            this.password = password;
+            return this;
+        }
+
+        public UserRequestDtoBuilder withOccupation(String occupation) {
+            this.occupation = occupation;
+            return this;
+        }
+
+        public UserRequestDto build() {
+            UserRequestDto userRequestDto = new UserRequestDto();
+            userRequestDto.password = this.password;
+            userRequestDto.email = this.email;
+            userRequestDto.username = this.username;
+            userRequestDto.lastName = this.lastName;
+            userRequestDto.occupation = this.occupation;
+            userRequestDto.firstName = this.firstName;
+            return userRequestDto;
+        }
+    }
+}
