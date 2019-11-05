df767e95f0fa30ed8a4cd3d3a0dbece6489ad16d
Sophia <montesbolahs@elsevier.com>
2019-11-05 11:14:52 +0000

    GP19-67/68/69: create user endpoint

--- SpringProject/src/main/java/com/gradproject2019/users/service/UserServiceImpl.java
+++ SpringProject/src/main/java/com/gradproject2019/users/service/UserServiceImpl.java
@@ -1,5 +1,8 @@
 package com.gradproject2019.users.service;
 
+import com.gradproject2019.users.data.UserRequestDto;
+import com.gradproject2019.users.data.UserResponseDto;
+import com.gradproject2019.users.persistance.User;
 import com.gradproject2019.users.repository.UserRepository;
 import org.springframework.stereotype.Service;
 
@@ -12,6 +15,11 @@ public class UserServiceImpl implements UserService {
         this.userRepository = userRepository;
     }
 
-    //@Override
-    //saveUser stuff
+    @Override
+    public UserResponseDto saveUser(UserRequestDto userRequestDto) {
+        User user = userRequestDto.from(userRequestDto);
+        //Check username and password
+        return new UserResponseDto().from(userRepository.saveAndFlush(user));
+
+    }
 }
