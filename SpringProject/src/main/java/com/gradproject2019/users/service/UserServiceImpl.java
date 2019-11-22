c5995fd9bb7e521e03dc4c15aecc1e616b161867
Karen <k.zamani-pickford@elsevier.com>
2019-11-22 13:25:55 +0000

    GP19-0 renamed exception from regex to credentials

--- SpringProject/src/main/java/com/gradproject2019/users/service/UserServiceImpl.java
+++ SpringProject/src/main/java/com/gradproject2019/users/service/UserServiceImpl.java
@@ -3,7 +3,7 @@ package com.gradproject2019.users.service;
 import com.gradproject2019.users.data.UserRequestDto;
 import com.gradproject2019.users.data.UserResponseDto;
 import com.gradproject2019.users.exception.UserInfoExistsException;
-import com.gradproject2019.users.exception.InvalidRegexFormatException;
+import com.gradproject2019.users.exception.InvalidCredentialsException;
 import com.gradproject2019.users.persistance.User;
 import com.gradproject2019.users.repository.UserRepository;
 import com.gradproject2019.utils.AuthUtils;
@@ -43,7 +43,7 @@ public class UserServiceImpl implements UserService {
 
     private void checkRegex(String input, String pattern) {
         if (!AuthUtils.validate(input, pattern)) {
-            throw new InvalidRegexFormatException();
+            throw new InvalidCredentialsException();
         }
     }
 
