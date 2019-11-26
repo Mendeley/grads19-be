f16db7aa4401f1e65d466d90ed0b72aa1e04f811
Sophia <montesbolahs@elsevier.com>
2019-11-26 14:12:47 +0000

    GP19-92: create patch endpoint and tests

--- SpringProject/src/main/java/com/gradproject2019/users/exception/UserNotFoundException.java
+++ SpringProject/src/main/java/com/gradproject2019/users/exception/UserNotFoundException.java
@@ -3,6 +3,6 @@ package com.gradproject2019.users.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(code = HttpStatus.CONFLICT, reason="Credentials are already in use.")
-public class UserInfoExistsException extends RuntimeException {
+@ResponseStatus(code = HttpStatus.NOT_FOUND, reason="User not found.")
+public class UserNotFoundException extends RuntimeException{
 }
