d21b93f2effac42960af870b5f7c8519872ceecd
MerrettM <m.merrett@elsevier.com>
2019-12-09 11:59:49 +0000

    GP19-120: change wording in forbidden error message

--- SpringProject/src/main/java/com/gradproject2019/users/exception/UserRecognisedButUnauthorizedException.java
+++ SpringProject/src/main/java/com/gradproject2019/users/exception/UserRecognisedButUnauthorizedException.java
@@ -3,6 +3,6 @@ package com.gradproject2019.users.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(code = HttpStatus.FORBIDDEN, reason="User unauthorized to perform action.")
+@ResponseStatus(code = HttpStatus.FORBIDDEN, reason="User forbidden to perform action.")
 public class UserRecognisedButUnauthorizedException extends RuntimeException {
 }
