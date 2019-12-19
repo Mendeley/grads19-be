e93fafff9f251b9d8c481fcea22fd8f62948e8b5
Sophia <montesbolahs@elsevier.com>
2019-12-19 14:26:19 +0000

    GP19-151: WIP integration test

--- SpringProject/src/it/java/com/gradproject2019/conferences/ElasticsearchIT.java
+++ SpringProject/src/it/java/com/gradproject2019/conferences/ElasticsearchIT.java
@@ -1,13 +1,18 @@
 package com.gradproject2019.conferences;
 
+import com.gradproject2019.conferences.persistance.EsConference;
 import com.gradproject2019.conferences.repository.ConferenceSearchRepository;
+import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.ClassRule;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.testcontainers.elasticsearch.ElasticsearchContainer;
+
+import java.time.Instant;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment= SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -22,4 +27,19 @@ public class ElasticsearchIT {
     public static void before() {
         System.setProperty("spring.data.elasticsearch.cluster-nodes", container.getContainerIpAddress() + ":" + container.getMappedPort(9300));
     }
+
+    @Test
+    public void shouldSaveEsConferenceToSearchRepository() {
+        EsConference esConference = new EsConference();
+        esConference.setId(1L);
+        esConference.setName("SophiaCon");
+        esConference.setDateTime(Instant.now());
+        esConference.setCity("London");
+        esConference.setDescription("A conference");
+        esConference.setTopic("Sophia");
+        esConference = repository.save(esConference);
+        Assert.assertNotNull(esConference);
+
+    }
+
 }
