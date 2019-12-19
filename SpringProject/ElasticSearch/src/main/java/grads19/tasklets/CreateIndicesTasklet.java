4b44ae1491cd0057d434d614faaa99d1be3bcbeb
Grace <g.burleyjones@elsevier.com>
2019-12-19 17:21:08 +0000

    GP19: added elastic search

---
+++ SpringProject/ElasticSearch/src/main/java/grads19/tasklets/CreateIndicesTasklet.java
@@ -0,0 +1,33 @@
+package grads19.tasklets;
+
+import grads19.services.ElasticSearchService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.batch.core.StepContribution;
+import org.springframework.batch.core.scope.context.ChunkContext;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CreateIndicesTasklet implements Tasklet {
+
+    private static final Logger LOG = LoggerFactory.getLogger(CreateIndicesTasklet.class);
+
+    @Autowired
+    private ElasticSearchService elasticSearchService;
+
+    @Override
+    public RepeatStatus execute(StepContribution stepContribution, ChunkContext chunkContext) throws Exception {
+
+        final var result = elasticSearchService.createIndices();
+
+        if (result) {
+            LOG.info("index sucessfully completed");
+        } else {
+            LOG.error("Index failed");
+        }
+        return RepeatStatus.FINISHED;
+    }
+}
