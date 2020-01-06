c823a601308a607e53a71436ee65468a89e205a9
Grace <g.burleyjones@elsevier.com>
2020-01-06 16:55:47 +0000

    GP19: ES and main app modules working

--- SpringProject/MainApplication/src/test/java/java/com/gradproject2019/UserConferenceServiceTest.java
+++ SpringProject/MainApplication/src/test/java/java/com/gradproject2019/UserConferenceServiceTest.java
@@ -1,75 +1,72 @@
 package java.com.gradproject2019;
 
 import gradproject2019.auth.persistence.Token;
+import gradproject2019.auth.service.AuthServiceImpl;
 import gradproject2019.conferences.data.ConferenceResponseDto;
-import gradproject2019.conferences.service.ConferenceService;
+import gradproject2019.conferences.persistence.Conference;
+import gradproject2019.conferences.service.ConferenceServiceImpl;
 import gradproject2019.userConference.data.UserConferenceRequestDto;
 import gradproject2019.userConference.data.UserConferenceResponseDto;
 import gradproject2019.userConference.persistence.UserConference;
 import gradproject2019.userConference.repository.UserConferenceRepository;
 import gradproject2019.userConference.service.UserConferenceServiceImpl;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 
+import java.time.Instant;
 import java.util.List;
 import java.util.UUID;
 
-import static gradproject2019.conferences.data.ConferenceResponseDto.ConferenceResponseDtoBuilder.aConferenceResponseDto;
-import static gradproject2019.userConference.persistence.UserConference.UserConferenceBuilder.anUserConference;
-import static java.time.Instant.now;
 import static org.assertj.core.api.Java6Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
 
 @RunWith(MockitoJUnitRunner.class)
 public class UserConferenceServiceTest {
+
     @InjectMocks
-    private UserConferenceServiceImpl userConferenceService;
+    private UserConferenceServiceImpl userConferenceServiceImpl;
 
     @Mock
     private UserConferenceRepository userConferenceRepository;
 
+    @Mock
+    private AuthServiceImpl authServiceImpl;
 
     @Mock
-    private ConferenceService conferenceService;
+    private ConferenceServiceImpl conferenceServiceImpl;
 
     private final Token token = new Token(1L, UUID.randomUUID());
-    private final UserConferenceRequestDto interest = new UserConferenceRequestDto(1L, 1L);
+    private final UserConferenceRequestDto userConferenceRequestDto = new UserConferenceRequestDto(1L, 1L);
+    private final UserConference userConference = new UserConference(1L, 1L);
+    private final Conference conference = new Conference(1L, "Grace's conference", Instant.now(), "Leicester", "All about Grace's fabulous and extra house", "grace");
+    private final ConferenceResponseDto conferenceResponseDto = new ConferenceResponseDto().from(conference);
 
     @Test
     public void shouldSaveInterestInConference() {
-        UserConference userConference = anUserConference()
-                .withUserId(1L)
-                .withConferenceId(1L)
-                .build();
+        given(authServiceImpl.getTokenById(token.getToken())).willReturn(token);
+        given(conferenceServiceImpl.getConferenceById(userConference.getConferenceId())).willReturn(conferenceResponseDto);
         given(userConferenceRepository.saveAndFlush(any(UserConference.class))).willReturn(userConference);
 
-        UserConferenceResponseDto userConferenceResponseDto = userConferenceService.saveInterest(token.getToken(), interest);
+        UserConferenceResponseDto userConferenceResponseDto = userConferenceServiceImpl.saveInterest(token.getToken(), userConferenceRequestDto);
 
         assertThat(userConference.getConferenceId()).isEqualTo(userConferenceResponseDto.getConferenceId());
         assertThat(userConference.getUserId()).isEqualTo(userConferenceResponseDto.getUserId());
-
     }
 
     @Test
     public void shouldGetConferenceUserIsInterestedIn() {
-        UserConference userConference = anUserConference()
-                .withUserId(1L)
-                .withConferenceId(1L)
-                .build();
+        given(authServiceImpl.getTokenById(token.getToken())).willReturn(token);
+        given(userConferenceRepository.findByUserId(userConference.getUserId())).willReturn(List.of(userConference));
+        given(conferenceServiceImpl.getConferenceById(userConference.getConferenceId())).willReturn(conferenceResponseDto);
 
-        ConferenceResponseDto conference = aConferenceResponseDto().withCity("Leciester").withDateTime(now()).withDescription("Hello").withId(1L).withName("Test").withTopic("home").build();
+        List<ConferenceResponseDto> conferenceResponseDtoList = userConferenceServiceImpl.getConferenceByUserId(token.getToken());
 
-        final List<UserConference> conferencesByUserId = List.of(userConference);
-        given(userConferenceRepository.findByUserId(1L)).willReturn(conferencesByUserId);
-        given(conferenceService.getConferenceById(1L)).willReturn(conference);
-
-        List<ConferenceResponseDto> conferences = userConferenceService.getConferenceByUserId(token.getToken());
-
-        assertThat(conferences).hasSize(1);
-        assertThat(conferences.get(0).getId()).isEqualTo(userConference.getConferenceId());
+        Assert.assertEquals(conferenceResponseDtoList.size(), 1);
+        assertThat(conferenceResponseDtoList.get(0).getId()).isEqualTo(userConference.getConferenceId());
     }
 }
