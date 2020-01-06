947a69997e95bebe9a3abcbc1b25952ac27695f1
Grace <g.burleyjones@elsevier.com>
2020-01-06 16:55:47 +0000

    GP19: ES and main app modules working

--- SpringProject/src/main/java/com/gradproject2019/auth/exception/InvalidCredentialsException.java
+++
@@ -1,8 +0,0 @@
-package com.gradproject2019.auth.exception;
-
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
-@ResponseStatus(code = HttpStatus.UNAUTHORIZED, reason = "Invalid user credentials.")
-public class InvalidCredentialsException extends RuntimeException {
-}
--- SpringProject/src/main/java/com/gradproject2019/conferences/exception/ConferenceNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.gradproject2019.conferences.exception;
-
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
-@ResponseStatus(code = HttpStatus.NOT_FOUND, reason = "Conference not found.")
-public class ConferenceNotFoundException extends RuntimeException {
-}
