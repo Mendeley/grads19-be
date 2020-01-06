68fe10981b79b758a19f7d0d59cd6bc0b29500f6
Grace <g.burleyjones@elsevier.com>
2020-01-06 16:55:47 +0000

    GP19: ES and main app modules working

--- SpringProject/MainApplication/src/main/java/gradproject2019/auth/service/AuthService.java
+++ SpringProject/MainApplication/src/main/java/gradproject2019/auth/service/AuthService.java
@@ -10,8 +10,6 @@ public interface AuthService {
     Token login(LoginDto loginDto);
 
     void logout(UUID token);
-  
-    void checkTokenExists(UUID token);
 
-    Token getTokenById(UUID token);
+    void checkTokenExists(UUID token);
 }
