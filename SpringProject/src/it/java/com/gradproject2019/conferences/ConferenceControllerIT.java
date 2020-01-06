3179acdd373691f14d4d0ae04cf53b07a2494feb
MerrettM <m.merrett@elsevier.com>
2020-01-06 14:04:54 +0000

    GP19-125: combine methods which return errors and add auth tests

--- SpringProject/src/it/java/com/gradproject2019/conferences/ConferenceControllerIT.java
+++ SpringProject/src/it/java/com/gradproject2019/conferences/ConferenceControllerIT.java
@@ -15,6 +15,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -91,14 +92,14 @@ public class ConferenceControllerIT extends TestUtils {
     @Test
     public void shouldReturn200AndSaveConferenceInDatabase() throws URISyntaxException {
         URI uri = new URI(baseUri);
-        ConferenceRequestDto conferenceRequestDto = createRequestDto(savedConference.getId(), "Grace's conference", Instant.parse("3000-12-30T19:34:50.63Z"), "Leicester", "All about Grace's fabulous and extra house", "grace");
+        ConferenceRequestDto request = createRequestDto(savedConference.getId(), "Grace's conference", Instant.parse("3000-12-30T19:34:50.63Z"), "Leicester", "All about Grace's fabulous and extra house", "grace");
 
 
-        ResponseEntity<String> response = postConference(uri, conferenceRequestDto);
+        ResponseEntity<String> response = postConference(uri, request);
         Conference retrievedConference = conferenceRepository.findAll().get(0);
 
         Assert.assertEquals(200, response.getStatusCodeValue());
-        Assert.assertEquals(conferenceRequestDto.getName(), retrievedConference.getName());
+        Assert.assertEquals(request.getName(), retrievedConference.getName());
     }
 
     @Test
@@ -106,7 +107,7 @@ public class ConferenceControllerIT extends TestUtils {
         URI uri = new URI(baseUri);
         ConferenceRequestDto request = createRequestDto(null, null, null, null, null, null);
 
-        ResponseEntity<ErrorEntity> response = postConferenceExpectingError(uri, request);
+        ResponseEntity<ErrorEntity> response = postConferenceExpectingError(uri, request, passingHeaders);
 
         Assert.assertEquals(400, response.getStatusCodeValue());
         Assert.assertEquals("Validation failed for object='conferenceRequestDto'. Error count: 5", response.getBody().getMessage());
@@ -117,17 +118,28 @@ public class ConferenceControllerIT extends TestUtils {
         URI uri = new URI(baseUri);
         ConferenceRequestDto request = createRequestDto(savedConference.getId(), "Grace's conference", Instant.parse("2018-12-30T19:34:50.63Z"), "Leicester", "All about Grace's fabulous and extra house", "grace");
 
-        ResponseEntity<ErrorEntity> response = postConferenceExpectingError(uri, request);
+        ResponseEntity<ErrorEntity> response = postConferenceExpectingError(uri, request, passingHeaders);
 
         Assert.assertEquals(400, response.getStatusCodeValue());
         Assert.assertEquals("Invalid entry in conference field.", response.getBody().getMessage());
     }
 
     @Test
+    public void shouldReturn401WhenUnauthorisedSave() throws URISyntaxException {
+        URI uri = new URI(baseUri);
+        ConferenceRequestDto request = createRequestDto(savedConference.getId(), "Grace's conference", Instant.parse("3000-12-30T19:34:50.63Z"), "Leicester", "All about Grace's fabulous and extra house", "grace");
+
+        ResponseEntity<ErrorEntity> response = postConferenceExpectingError(uri, request, failingHeaders);
+
+        Assert.assertEquals(401, response.getStatusCodeValue());
+        Assert.assertEquals("User unauthorized to perform action.", response.getBody().getMessage());
+    }
+
+    @Test
     public void shouldReturn404WhenConferenceToBeDeletedNotFound() throws URISyntaxException {
         URI uri = new URI(baseUri + "/1000000000");
 
-        ResponseEntity<ErrorEntity> response = deleteConferenceExpectingError(uri);
+        ResponseEntity<ErrorEntity> response = deleteConferenceExpectingError(uri, passingHeaders);
 
         Assert.assertEquals(404, response.getStatusCodeValue());
         Assert.assertEquals("Conference not found.", response.getBody().getMessage());
@@ -148,7 +160,7 @@ public class ConferenceControllerIT extends TestUtils {
     public void shouldReturn401WhenUnauthorisedDelete() throws URISyntaxException {
         URI uri = new URI(baseUri + "/" + savedConference.getId());
 
-        ResponseEntity<ErrorEntity> response = deleteConferenceExpectingAuthError(uri);
+        ResponseEntity<ErrorEntity> response = deleteConferenceExpectingError(uri, failingHeaders);
 
         Assert.assertEquals(401, response.getStatusCodeValue());
         Assert.assertEquals("User unauthorized to perform action.", response.getBody().getMessage());
@@ -160,10 +172,21 @@ public class ConferenceControllerIT extends TestUtils {
         URI uri = new URI(baseUri + "/1000000000");
         ConferencePatchRequestDto request = createPatchRequestDto(null, null, null, null, null);
 
-        ResponseEntity<ErrorEntity> response = editConferenceExpectingError(uri, request);
+        ResponseEntity<ErrorEntity> response = editConferenceExpectingError(uri, request, passingHeaders);
 
         Assert.assertEquals(404, response.getStatusCodeValue());
         Assert.assertEquals("Conference not found.", response.getBody().getMessage());
+    }
+
+    @Test
+    public void shouldReturn401WhenUnauthorisedEdit() throws URISyntaxException {
+        URI uri = new URI(baseUri + "/" + savedConference.getId());
+        ConferencePatchRequestDto request = createPatchRequestDto(null, null, null, null, null);
+
+        ResponseEntity<ErrorEntity> response = editConferenceExpectingError(uri, request, failingHeaders);
+
+        Assert.assertEquals(401, response.getStatusCodeValue());
+        Assert.assertEquals("User unauthorized to perform action.", response.getBody().getMessage());
     }
 
     @Test
@@ -202,8 +225,8 @@ public class ConferenceControllerIT extends TestUtils {
         });
     }
 
-    private ResponseEntity<ErrorEntity> postConferenceExpectingError(URI uri, ConferenceRequestDto request) {
-        return restTemplate.exchange(uri, POST, new HttpEntity<>(request, passingHeaders), ErrorEntity.class);
+    private ResponseEntity<ErrorEntity> postConferenceExpectingError(URI uri, ConferenceRequestDto request, HttpHeaders headers) {
+        return restTemplate.exchange(uri, POST, new HttpEntity<>(request, headers), ErrorEntity.class);
     }
 
     private ResponseEntity<String> deleteConference(URI uri) {
@@ -211,12 +234,8 @@ public class ConferenceControllerIT extends TestUtils {
         });
     }
 
-    private ResponseEntity<ErrorEntity> deleteConferenceExpectingError(URI uri) {
-        return restTemplate.exchange(uri, DELETE, new HttpEntity<>(passingHeaders), ErrorEntity.class);
-    }
-
-    private ResponseEntity<ErrorEntity> deleteConferenceExpectingAuthError(URI uri) {
-        return restTemplate.exchange(uri, DELETE, new HttpEntity<>(failingHeaders), ErrorEntity.class);
+    private ResponseEntity<ErrorEntity> deleteConferenceExpectingError(URI uri, HttpHeaders headers) {
+        return restTemplate.exchange(uri, DELETE, new HttpEntity<>(headers), ErrorEntity.class);
     }
 
     private ResponseEntity<ConferenceResponseDto> editConference(URI uri, ConferencePatchRequestDto request) {
@@ -224,8 +243,8 @@ public class ConferenceControllerIT extends TestUtils {
         });
     }
 
-    private ResponseEntity<ErrorEntity> editConferenceExpectingError(URI uri, ConferencePatchRequestDto request) {
-        return restTemplate.exchange(uri, PATCH, new HttpEntity<>(request, passingHeaders), ErrorEntity.class);
+    private ResponseEntity<ErrorEntity> editConferenceExpectingError(URI uri, ConferencePatchRequestDto request, HttpHeaders headers) {
+        return restTemplate.exchange(uri, PATCH, new HttpEntity<>(request, headers), ErrorEntity.class);
     }
 
     private ConferenceRequestDto createRequestDto(Long id, String name, Instant dateTime, String city, String description, String topic) {
