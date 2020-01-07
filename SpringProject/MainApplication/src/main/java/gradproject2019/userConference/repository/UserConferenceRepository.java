dc230f3fb28aadf4e734d565545f09a7e6dd21b7
Grace <g.burleyjones@elsevier.com>
2020-01-07 13:58:20 +0000

    GP19: Both main application and ES working

--- SpringProject/MainApplication/src/main/java/gradproject2019/userConference/repository/UserConferenceRepository.java
+++ SpringProject/MainApplication/src/main/java/gradproject2019/userConference/repository/UserConferenceRepository.java
@@ -2,6 +2,7 @@ package gradproject2019.userConference.repository;
 
 import gradproject2019.userConference.persistence.UserConference;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
