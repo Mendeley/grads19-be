139291bd2e803896f094a5e81d22722fad3e6856
Grace <g.burleyjones@elsevier.com>
2020-01-09 12:17:06 +0000

    GP19-175: renamed auth exception due to duplicate

--- SpringProject/MainApplication/src/main/java/gradproject2019/auth/exception/InvalidCredentialsAuthException.java
+++ SpringProject/MainApplication/src/main/java/gradproject2019/auth/exception/InvalidCredentialsAuthException.java
@@ -4,5 +4,5 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(code = HttpStatus.UNAUTHORIZED, reason = "Invalid user credentials.")
-public class InvalidCredentialsException extends RuntimeException {
+public class InvalidCredentialsAuthException extends RuntimeException {
 }
