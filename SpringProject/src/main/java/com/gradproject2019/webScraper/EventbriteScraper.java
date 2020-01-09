4ed8764bf010034b4ff9a3f0bfd30413a35a208c
Karen <k.zamani-pickford@elsevier.com>
2020-01-09 10:30:08 +0000

    GP19-133 WIP eventbrite scraper

--- SpringProject/src/main/java/com/gradproject2019/webScraper/EventbriteScraper.java
+++ SpringProject/src/main/java/com/gradproject2019/webScraper/EventbriteScraper.java
@@ -28,79 +28,75 @@ public class EventbriteScraper extends WebCrawler {
         String url = page.getWebURL().getURL();
         logger.info("URL: {}", url);
 
-            //***Pattern Matcher Version***
-
         if (page.getParseData() instanceof HtmlParseData) {
 
             HtmlParseData htmlParseData = (HtmlParseData) page.getParseData();
 
             String html = htmlParseData.getHtml();
 
-            Pattern titlePattern = Pattern.compile("<h1([^>]*)title(.*)>(.*)</h1>");
-            Matcher titleMatcher = titlePattern.matcher(html);
+            if (html != null ) {
 
-            if (titleMatcher.find()) {
-                logger.info("Match");
-                String tester = titleMatcher.group(3);
-                logger.info("Title: {}", tester);
+                Pattern titlePattern = Pattern.compile("<h1([^>]*)title(.*)>(.*)</h1>");
+                Matcher titleMatcher = titlePattern.matcher(html);
 
-            } else {
-                logger.info("No title match");
+                if (titleMatcher.find()) {
+                    logger.info("Match");
+                    String scrapedConferenceTitle = titleMatcher.group(3);
+                    logger.info("Title: {}", scrapedConferenceTitle);
+
+                } else {
+                    logger.info("No title match");
+                }
+
+                Pattern dateTimePattern = Pattern.compile("<time([^>]*)>(.*)</time>");
+                Matcher dateTimeMatcher = dateTimePattern.matcher(html);
+
+                if (dateTimeMatcher.find()) {
+                    logger.info("Match");
+                    String scrapedDateTime = dateTimeMatcher.group(2);
+                    logger.info("dateTime: {}", scrapedDateTime);
+
+                } else {
+                    logger.info("No dateTime match");
+                }
+
+                Pattern timePattern = Pattern.compile("^[0-2][0-9]:[0-5][0-9]$");
+                Matcher timeMatcher = timePattern.matcher(html);
+
+                if (timeMatcher.find()) {
+                    logger.info("Match");
+                    String scrapedTime = timeMatcher.group(2);
+                    logger.info("Time: {}", scrapedTime);
+
+                } else {
+                    logger.info("No time match");
+                }
+
+                Pattern cityPattern = Pattern.compile("<div([^>]*)event-details__data(.*)>(.*)</div>");
+                Matcher cityMatcher = cityPattern.matcher(html);
+
+                if (cityMatcher.find()) {
+                    logger.info("Match");
+                    String scrapedCity = cityMatcher.group(3);
+                    logger.info("City: {}", scrapedCity);
+
+                } else {
+                    logger.info("No city match");
+                }
+
+                Pattern descriptionPattern = Pattern.compile("<div([^>]*)content(.*)>(.*)</div>");
+                Matcher descriptionMatcher = descriptionPattern.matcher(html);
+
+                if (descriptionMatcher.find()) {
+                    logger.info("Match");
+                    String scrapedDescription = descriptionMatcher.group(3);
+                    logger.info("Description: {}", scrapedDescription);
+
+                } else {
+                    logger.info("No description match");
+                }
             }
-
-            Pattern timePattern = Pattern.compile("<time([^>]*)>(.*)</time>");
-            Matcher timeMatcher = timePattern.matcher(html);
-
-            if (timeMatcher.find()) {
-                logger.info("Match");
-                String tester = timeMatcher.group(2);
-                logger.info("Time: {}", tester);
-
-            } else {
-                logger.info("No time match");
-            }
-
-            Pattern cityPattern = Pattern.compile("<div([^>]*)event-details__data(.*)>(.*)</div>");
-            Matcher cityMatcher = cityPattern.matcher(html);
-
-            if (cityMatcher.find()) {
-                logger.info("Match");
-                String tester = cityMatcher.group(3);
-                logger.info("City: {}", tester);
-
-            } else {
-                logger.info("No city match");
-            }
-
-            Pattern descriptionPattern = Pattern.compile("<div([^>]*)content(.*)>(.*)</div>");
-            Matcher descriptionMatcher = descriptionPattern.matcher(html);
-
-            if (descriptionMatcher.find()) {
-                logger.info("Match");
-                String tester = descriptionMatcher.group(3);
-                logger.info("Description: {}", tester);
-
-            } else {
-                logger.info("No description match");
-            }
-
-
-
-//
-//            if (text != null && html != null && titleMatcher.find()) {
-//                String codeGroup = titleMatcher.group(1);
-//                logger.info("Title: {}", codeGroup);
-//
-//                String scrapedConferenceTitle = "example title";
-//                Instant scrapedDateTime = Instant.now();
-//                String scrapedCity = "example city";
-//                String scrapedDescription = "example description";
-//                String scrapedTopic = "example topic";
-//
-//                scraperOutput = new ScraperOutput(scrapedConferenceTitle, scrapedDateTime, scrapedCity, scrapedDescription, scrapedTopic);
-//            }
         }
-
         //TODO: Ensure that the scraper output is not created if values are null or there's an exception
     }
 
