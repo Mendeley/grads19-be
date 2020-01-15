7a02531183ab2c01bd5546223a86dd923453cd4e
Grace <g.burleyjones@elsevier.com>
2020-01-15 19:29:18 +0000

    GP19 topic search working

---
+++ SpringProject/MainApplication/src/main/java/gradproject2019/elasticsearch/persistence/EsConference.java
@@ -0,0 +1,82 @@
+package gradproject2019.elasticsearch.persistence;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.elasticsearch.annotations.Document;
+
+import java.time.Instant;
+
+@Document(indexName = "conferences", type = "_doc")
+public class EsConference {
+    @Id
+    private Long id;
+    private String name;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern ="yyyy-MM-dd'T'HH:mm:ss.SSSZZ", timezone = "UTC")
+    private Instant dateTime;
+
+    private String city;
+    private String description;
+    private String topic;
+
+    public EsConference() {
+    }
+
+    public EsConference(Long id, String name, Instant dateTime, String city, String description, String topic) {
+        this.id = id;
+        this.name = name;
+        this.dateTime = dateTime;
+        this.city = city;
+        this.description = description;
+        this.topic = topic;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Instant getDateTime() {
+    return dateTime;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getTopic() {
+        return topic;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setDateTime(Instant dateTime) {
+        this.dateTime = dateTime;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setTopic(String topic) {
+        this.topic = topic;
+    }
+
+}
---
+++ SpringProject/MainApplication/src/main/java/gradproject2019/elasticsearch/persistence/SearchConfig.java
@@ -0,0 +1,120 @@
+
+package gradproject2019.elasticsearch.persistence;
+
+
+import org.elasticsearch.client.RestHighLevelClient;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.elasticsearch.client.ClientConfiguration;
+import org.springframework.data.elasticsearch.client.RestClients;
+import org.springframework.data.elasticsearch.core.ElasticsearchOperations;
+import org.springframework.data.elasticsearch.core.ElasticsearchRestTemplate;
+import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
+
+
+
+@Configuration
+@EnableElasticsearchRepositories(basePackages = "com.gradproject2019.conferences.repository")
+@ComponentScan(basePackages = "com.gradproject2019.conferences.service")
+public class SearchConfig {
+    @Value("${spring.data.elasticsearch.cluster-nodes}")
+    private String esHost;
+
+    @Bean
+    public RestHighLevelClient client() {
+        ClientConfiguration clientConfiguration = ClientConfiguration.builder()
+                .connectedTo(esHost)
+                .build();
+
+        return RestClients.create(clientConfiguration).rest();
+    }
+
+    @Bean
+    public ElasticsearchOperations elasticsearchTemplate() {
+        return new ElasticsearchRestTemplate(client());
+    }
+}
+
+
+
+//    @Bean
+//    @Override
+//    public EntityMapper EntityMapper() {
+//        ElasticsearchEntityMapper entityMapper = new ElasticsearchEntityMapper(
+//                elasticsearchMappingContext(), new DefaultConversionService());
+//        entityMapper.setConversions(elasticsearchCustomConversions());
+//
+//        return entityMapper;
+//    }
+//
+//    @Bean
+//    @Override
+//    public ElasticsearchCustomConversions elasticsearchCustomConversions() {
+//        return new ElasticsearchCustomConversions(
+//                Arrays.asList(new AddressToMap(), new MapToAddress()));
+//    }
+//
+//    @WritingConverter
+//    static class AddressToMap implements Converter<Address, Map<String, Object>> {
+//
+//        @Override
+//        public Map<String, Object> convert(Address source) {
+//
+//            LinkedHashMap<String, Object> target = new LinkedHashMap<>();
+//            target.put("ciudad", source.getCity());
+//            // ...
+//
+//            return target;
+//        }
+//    }
+//
+//    @ReadingConverter
+//    static class MapToAddress implements Converter<Map<String, Object>, Address> {
+//
+//        @Override
+//        public Address convert(Map<String, Object> source) {
+//
+//            // ...
+//            return address;
+//        }
+//    }
+//
+//
+//
+//
+//
+//        @Bean
+//    @Primary
+//    public ElasticsearchOperations elasticsearchTemplate(final JestClient jestClient,
+//                                                         final ElasticsearchConverter elasticsearchConverter,
+//                                                         final SimpleElasticsearchMappingContext simpleElasticsearchMappingContext, EntityMapper mapper) {
+//        return new JestElasticsearchTemplate(jestClient, elasticsearchConverter,
+//                new DefaultJestResultsMapper(simpleElasticsearchMappingContext, mapper));
+//    }
+//
+//    public class CustomEntityMapper implements EntityMapper {
+//
+//        private ObjectMapper mapper;
+//
+//        @Autowired
+//        public CustomEntityMapper() {
+//            this.mapper = new ObjectMapper();
+//            mapper.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
+//            mapper.disable(DeserializationFeature.ACCEPT_SINGLE_VALUE_AS_ARRAY, true);
+//            mapper.registerModule(new JavaTimeModule());
+//        }
+//
+//        @Override
+//        public String mapToString(Object object) throws IOException {
+//            return mapper.writeValueAsString(object);
+//        }
+//
+//        @Override
+//        public <T> T mapToObject(String source, Class<T> clazz) throws IOException {
+//            return mapper.readValue(source, clazz);
+//        }
+//
+//    }
+//}
